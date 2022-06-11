a62a796b85edd47d1d9ec15570bfae28f7ecd998
Alice Phan <alice.p5498@gmail.com>
2022-06-11 22:54:51 +0000

    Change PORT

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/comments/:date", getComment);
 connection.once('open', ()=>{
    console.log('connected to db');
   
-    const server = app.listen(80, ()=>{
+    const server = app.listen(8080, ()=>{
         console.log(`App Backend is running`);
     });
 }); 
